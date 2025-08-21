da25d77f8110bfec152a02cab6710e04182d3b44
abdullatif <abdullatifjr002@gmail.com>
2025-08-21 21:56:21 +0300

    updated LoadingSpinner.tsx

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,8 +1,13 @@
+import { FaSpinner } from "react-icons/fa6";
+import Logo from "./../../assets/Logo/Logo.png";
 
 const LoadingSpinner = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <div className="fixed flex flex-col gap-4 items-center justify-center h-screen w-screen bg-white">
+      <img src={Logo} alt="Loading..." className="animate-pulse" />
+      <FaSpinner className="text-xl text-primary-500 animate-spin mb-4 mx-auto" />
+    </div>
+  );
+};
 
-export default LoadingSpinner
+export default LoadingSpinner;
